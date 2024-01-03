fba5fd6f1240ee99d339155415df5d9c125ec043
ncb <nathancimbela006@gmail.com>
2024-01-03 13:48:11 +0100

    routing learning

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+//Route::get('/user', [UserController::class, 'index']);
